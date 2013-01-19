e8e4ed1ca1cc7c60bea5c7b396db76d5ef3a82da
Gerald <ghaagsma@umich.edu>
2013-01-19 13:53:24 -0500

    PhysObjects

--- Fired/Fired/Hero.cs
+++ Fired/Fired/Hero.cs
@@ -1,12 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace Fired
 {
     class Hero : Character
     {
 
+
+        public Hero(int x, int y) :
+            base(x, y)
+        {
+
+        }
+
+        // Load the object content
+        public override void load(ContentManager content)
+        {
+
+        }
+
+        // Update object
+        public override void update()
+        {
+
+        }
+
+        // Draw object
+        public override void draw()
+        {
+
+        }
+
+        // Handle collisions with map tiles
+        public override void mapCollide(Tile[,] tiles)
+        {
+
+        }
+
+        // Handle collisions with other objects in map
+        public override void objectCollide(PhysObject other)
+        {
+
+        }
     }
 }
