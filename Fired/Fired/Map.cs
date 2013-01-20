cc099580b3189b1be2edb0b604514eb6123a3438
Anthony Tesija <atesija@umich.edu>
2013-01-19 23:35:01 -0500

    Map changes for boss and sucker

--- Fired/Fired/Map.cs
+++ Fired/Fired/Map.cs
@@ -20,16 +20,19 @@ namespace Fired
         int level;
         Tile[,] tiles;
         Texture2D tileset, playerImage, guardImage, employeeImage, swatImage, sewageImage, bossImage;
-        bool levelFinished, loseGame, winGame;
+        bool levelFinished, loseGame, winGame, openWindow;
         Hero hero;
         List<Employee> employees;
         List<Swat> swat;
         List<SecurityGuard> guard;
+        List<Boss> boss;
+        List<SewageSucker> sucker;
         Rectangle stairs, window;
+        Vector2 windowLocation;
 
         public Map()
         {
-            level = 0;
+            level = 5;
             tiles = new Tile[MAP_ROWS, MAP_COLS];
             for (int i = 0; i < MAP_ROWS; ++i)
                 for (int j = 0; j < MAP_COLS; ++j)
@@ -38,11 +41,14 @@ namespace Fired
                     tiles[i, j].location = new Rectangle(j * Fired.TILE_SIZE, i * Fired.TILE_SIZE, Fired.TILE_SIZE, Fired.TILE_SIZE);
                 }
             levelFinished = true;
+            openWindow = false;
             loseGame = winGame = false;
             hero = new Hero(0, 0, 3);
             employees = new List<Employee>();
             swat = new List<Swat>();
             guard = new List<SecurityGuard>();
+            boss = new List<Boss>();
+            sucker = new List<SewageSucker>();
             stairs = new Rectangle();
             window = new Rectangle(2000, 2000, 1, 1);
         }
@@ -51,7 +57,7 @@ namespace Fired
         {
             tileset = content.Load<Texture2D>("tiles");
             playerImage = content.Load<Texture2D>("HeroStrip");
-            employeeImage = content.Load<Texture2D>("BusinessWoman");
+            employeeImage = content.Load<Texture2D>("CorporateWhore");
             guardImage = content.Load<Texture2D>("SecurityGuard");
             swatImage = content.Load<Texture2D>("Swat");
             sewageImage = content.Load<Texture2D>("SewageSucker");
@@ -99,11 +105,26 @@ namespace Fired
                 }
             }
 
+            //Update boss
+            for (int i = 0; i < boss.Count; ++i)
+            {
+                boss[i].update(tiles, hero.getPosition());
+                if (hero.getHitBox().Intersects(boss[i].getHitBox()))
+                {
+                    boss.RemoveAt(i);
+                    i--;
+                    openWindow = true;
+                    tiles[(int)windowLocation.Y, (int)windowLocation.X].imageSource = new Rectangle(Fired.SOURCE_SIZE * 8, 0, Fired.SOURCE_SIZE, Fired.SOURCE_SIZE);
+                }
+            }
+
             //Check if you can go up a level
             if (employees.Count == 0 && hero.getHitBox().Intersects(stairs))
                 levelFinished = true;
                 
-            //Check 
+            //Check player intersect with open window
+            if (hero.getHitBox().Intersects(window) && openWindow)
+                winGame = true;
         }
 
         public void Draw(SpriteBatch spriteBatch)
@@ -125,6 +146,12 @@ namespace Fired
 
             for (int i = 0; i < swat.Count; ++i)
                 swat[i].draw(spriteBatch);
+
+            for (int i = 0; i < boss.Count; ++i)
+                boss[i].draw(spriteBatch);
+
+            for (int i = 0; i < sucker.Count; ++i)
+                sucker[i].draw(spriteBatch);
 
             hero.draw(spriteBatch);
         }
@@ -189,7 +216,8 @@ namespace Fired
                     {
                         tiles[i, j].type = TileType.Collision;
                         tiles[i, j].imageSource = new Rectangle(Fired.SOURCE_SIZE * 7, 0, Fired.SOURCE_SIZE, Fired.SOURCE_SIZE);
-                        window = new Rectangle(j * Fired.TILE_SIZE, i * Fired.TILE_SIZE, Fired.TILE_SIZE, Fired.TILE_SIZE);
+                        window = new Rectangle(j * Fired.TILE_SIZE, i * Fired.TILE_SIZE, Fired.TILE_SIZE, Fired.TILE_SIZE + 5);
+                        windowLocation = new Vector2(i, j);
                     }
                 }
             }
@@ -237,8 +265,29 @@ namespace Fired
                 swat.Add(sw);
             }
 
-            //Add sewage
+            //Add boss
+            Boss b;
+            line = sr.ReadLine().Split(' ');
+            count = int.Parse(line[2]);
+            for (int i = 0; i < count; ++i)
+            {
+                line = sr.ReadLine().Split(' ');
+                b = new Boss(int.Parse(line[0]), int.Parse(line[1]), int.Parse(line[2]));
+                b.load(content, bossImage);
+                boss.Add(b);
+            }
 
+            //Add Sucker
+            SewageSucker su;
+            line = sr.ReadLine().Split(' ');
+            count = int.Parse(line[2]);
+            for (int i = 0; i < count; ++i)
+            {
+                line = sr.ReadLine().Split(' ');
+                su = new SewageSucker(int.Parse(line[0]), int.Parse(line[1]));
+                su.load(content, bossImage);
+                sucker.Add(su);
+            }
 
             sr.Close();
         }
